cfc38317e2086b16cd3af482d33fa901e9b2aed7
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-04-30 16:35:23 +0100

    Minimum viable product completed, small improvement

--- src/components/ContactsStack.js
+++ src/components/ContactsStack.js
@@ -5,9 +5,10 @@ import {Container, Nav} from "react-bootstrap";
 export default function ContactsStack(props) {
   return (
     <Container>
+        <h1 className="display-5">My GitHub Profile is at the link bellow:</h1>
       <Nav className="justify-content-center" activeKey="/home">
         <Nav.Item>
-          <Nav.Link href="https://github.com/vystartasv">My Github profile is here</Nav.Link>
+          <Nav.Link href="https://github.com/vystartasv">Thanks for checking it out:)</Nav.Link>
         </Nav.Item>
       </Nav>
     </Container>
--- src/components/ProjectsStack.js
+++ src/components/ProjectsStack.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import {Container, Figure, Nav} from "react-bootstrap";
+import {
+  Container, Figure, Nav, Row, Col,
+} from 'react-bootstrap';
 // import { Link } from 'react-router-dom';
 
 export default function ProjectsStack() {
   return (
-      <Container>
-        <Figure fluid>
-          <Figure.Image
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <Figure fluid>
+            <Figure.Image
               width={500}
               alt="screenshot of the project"
-              src={require(`./../common/images/kodflix-project.png`)}
-          />
-          <Figure.Caption className="lead">
+              src={require('./../common/images/kodflix-project.png')}
+            />
+            <Figure.Caption className="lead">
             Kodflix project
-          </Figure.Caption>
-          <Nav className="justify-content-center" activeKey="/home">
-            <Nav.Item>
-              <Nav.Link href="https://kodflix-by-vilius.herokuapp.com">Live application</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="https://github.com/vystartasv/kodflix">GitHub repository here</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Figure>
-      </Container>
+            </Figure.Caption>
+            <Nav className="justify-content-center" activeKey="/home">
+              <Nav.Item>
+                <Nav.Link href="https://kodflix-by-vilius.herokuapp.com">Live application</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="https://github.com/vystartasv/kodflix">GitHub repository here</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Figure>
+        </Col>
+      </Row>
+    </Container>
   );
 }
