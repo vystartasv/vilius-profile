4e8d0d7432ae01060f00768c5e4b800f619e63fd
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-05-01 17:18:42 +0100

    Navbar css style updated

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,12 +5,16 @@ export default class Menu extends React.Component {
   render() {
     return (
       <div>
-        <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="/vilius-profile/#/">Home</Navbar.Brand>
+        <Navbar
+          bg="primary"
+          variant="dark"
+          className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm"
+        >
+          <Navbar.Brand href="/vilius-profile/#/" className="p-1 text-dark">Home</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="/vilius-profile/#/projects">Projects</Nav.Link>
-            <Nav.Link href="/vilius-profile/#/about-me">About Me</Nav.Link>
-            <Nav.Link href="/vilius-profile/#/contacts">Contacts</Nav.Link>
+            <Nav.Link href="/vilius-profile/#/projects" className="p-2 text-dark">Projects</Nav.Link>
+            <Nav.Link href="/vilius-profile/#/about-me" className="p-2 text-dark">About Me</Nav.Link>
+            <Nav.Link href="/vilius-profile/#/contacts" className="p-2 text-dark">Contacts</Nav.Link>
           </Nav>
         </Navbar>
       </div>
