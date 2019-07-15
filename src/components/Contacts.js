3e411f3e558ba28bdd55b5175c8be2e5b0c1a312
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-07-15 21:10:15 +0100

    Added email address in contacts

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Container, Nav } from 'react-bootstrap';
 import ContactsStack from './ContactsStack';
 
-export default class Contacts extends React.Component {
-  render() {
-    return (
-      <ContactsStack class="card mb-4 shadow-sm rounded" />
-    );
-  }
+export default function Contacts() {
+  const contacts = [{ link: 'https://www.linkedin.com/in/vystartas/', label: 'Linkedin' },
+    { link: 'https://github.com/vystartasv', label: 'GitHub' }];
+  return (
+    <ContactsStack class="card mb-4 shadow-sm rounded" contacts={contacts} />
+  );
 }
--- src/components/ContactsStack.js
+++ src/components/ContactsStack.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import { Container, Nav } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Container, Nav, Button } from 'react-bootstrap';
 
-export default function ContactsStack(props) {
+
+export default function ContactsStack({ contacts }) {
+  const [isVisible, setIsVisible] = useState(false);
   return (
-    <Container className="bg-light rounded">
-      <h1 className="display-5">Linkedin</h1>
-      <Nav className="justify-content-center" activeKey="/home">
+    <Container className="bg-light rounded p-4">
+      {contacts.map(({ link, label }) => <ContactLinks className='p-5' link={link} label={label} />)}
+      {
+          !isVisible
+            ? (
+              <Button
+                className="btn btn-lg btn-primary"
+                onClick={() => setIsVisible(!isVisible)}
+              >
+              Show email
+              </Button>
+            )
+            : null
+      }
+      {isVisible ? (
+        <a href="mailto:vilius.vystartas@gmail.com?
+        subject=Vilius%2C%20I%20stumbled%20upon%20your%20portfolio%20and%20I%20would%20like%20to%20contact%20you&amp;
+        body=Hi%20Vilius%2C%0A%0AI%20am%20...%20and%20I%20would%20like%20to%20..."
+           className='display-3 text-main m-4'
+        >
+          vilius.vystartas@gmail.com
+        </a>
+      )
+        : <></>}
+    </Container>
+  );
+}
+
+function ContactLinks({link, label }) {
+  return (
+    <>
+      <Nav className="justify-content-center display-3">
         <Nav.Item>
-          <Nav.Link href="https://www.linkedin.com/in/vystartas/">Link to Linkedin</Nav.Link>
+          <a href={link}>{label}</a>
         </Nav.Item>
       </Nav>
-        <h1 className="display-5">Github</h1>
-        <Nav className="justify-content-center" activeKey="/home">
-            <Nav.Item>
-                <Nav.Link href="https://github.com/vystartasv">Link to Github</Nav.Link>
-            </Nav.Item>
-        </Nav>
-    </Container>
+    </>
   );
 }
